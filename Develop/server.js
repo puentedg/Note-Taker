5337e38b5712d2e74084dd014d3a6f63c6fa0d9c
puentedg <puentedg@gmail.com>
2022-10-19 08:42:24 -0400

    initial commit, files created

---
+++ Develop/server.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+// Wildcard route to direct users to a html page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Note-Taker
-application that can be used to write and save notes
+# Note Taker Starter Code
